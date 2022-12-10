39e007dfdd9e6468b8d9d46ea491b706b7fbd538
Federico Marquez <17768182+fefu-marquez@users.noreply.github.com>
2022-12-09 21:03:15 -0300

    Feat: Create form and basic structure

--- src/app/modules/expenses-manager/create-cash-flow/create-cash-flow.page.ts
+++ src/app/modules/expenses-manager/create-cash-flow/create-cash-flow.page.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
 
+type ShortDate = 'today' | 'tomorrow' | 'lastSelected';
 @Component({
   selector: 'app-create-cash-flow',
   templateUrl: './create-cash-flow.page.html',
   styleUrls: ['./create-cash-flow.page.scss'],
 })
 export class CreateCashFlowPage implements OnInit {
+  form: UntypedFormGroup = this.formBuilder.group({
+    amount: [0, [Validators.required, Validators.min(0.01)]],
+    date: [],
+    comment: ['']
+  });
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
   }
 
+  pickDate(shortDate: ShortDate) {
+    switch(shortDate) {
+      
+    }
+  }
 }
