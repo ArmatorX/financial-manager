fea439b1b4ecb8d1e4fa5e5082028eee80855488
Federico Marquez <17768182+fefu-marquez@users.noreply.github.com>
2022-12-11 15:20:56 -0300

    Feat: Polish styles, add functionality to date picker buttons

--- src/app/modules/expenses-manager/create-cash-flow/create-cash-flow.page.ts
+++ src/app/modules/expenses-manager/create-cash-flow/create-cash-flow.page.ts
@@ -1,27 +1,58 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
+import { IonDatetime, ModalController } from '@ionic/angular';
 
-type ShortDate = 'today' | 'tomorrow' | 'lastSelected';
+type ShortDate = 'today' | 'yesterday' | 'lastSelected';
 @Component({
   selector: 'app-create-cash-flow',
   templateUrl: './create-cash-flow.page.html',
   styleUrls: ['./create-cash-flow.page.scss'],
 })
 export class CreateCashFlowPage implements OnInit {
+  hasClickedCustomButtons = false;
   form: UntypedFormGroup = this.formBuilder.group({
-    amount: [0, [Validators.required, Validators.min(0.01)]],
-    date: [],
+    amount: ['', [Validators.required, Validators.min(0.01)]],
+    date: [''],
     comment: ['']
   });
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder, private modalController: ModalController) { }
 
   ngOnInit() {
+    this.form.patchValue({
+      amount: '2.00',
+      date: '1996-11-24',
+      comment: 'Conchasumadre'
+    })
   }
 
   pickDate(shortDate: ShortDate) {
+    // TODO: When you re-open the datepicker modal, it doesn't show the selected date
+    this.hasClickedCustomButtons = true;
+    const today = new Date();
     switch(shortDate) {
-      
+      case 'today':
+        this.form.patchValue({ date: today.toJSON().slice(0,10) });
+        break;
+      case 'yesterday':
+        today.setDate(today.getDate() - 1);
+        this.form.patchValue({ date: today.toJSON().slice(0,10) });
+        break;
+    }
+    this.modalController.dismiss();
+  }
+
+  async selectDate(datepicker: IonDatetime) {
+    // TODO: Doesn't work when you change the month and pick the same date
+    if (this.hasClickedCustomButtons) {
+      this.hasClickedCustomButtons = false;
+      datepicker.reset();
+      return;
+    }
+    const previousDayValue = datepicker.value.slice(-2);
+    await datepicker.confirm();
+    if (previousDayValue !== datepicker.value.slice(-2)) {
+      this.modalController.dismiss();
     }
   }
 }
